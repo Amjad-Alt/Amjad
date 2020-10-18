bf6a5b15435052ca3faf566b16a6ddae7675dd31
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2020-10-18 21:04:47 +0300

    New

--- Womens_colthes_analysis.R
+++ Womens_colthes_analysis.R
@@ -12,7 +12,7 @@ setwd("C:/Users/amjad/Documents/Misk/Amjad")
 
 
 
-# What is the mean rate?
+# What is the mean of the rate?
 ## Change from integer to numeric 
 Rating <- as.numeric(clothes$Rating)
 
@@ -20,29 +20,25 @@ Rating <- as.numeric(clothes$Rating)
 rating_mean <-mean(sum(Rating)/length(Rating))
 
 
-# How age of buyers affect their rating?
+# Does age of buyers affect their rating?
 ##change values into numeric
 Age <- as.numeric(clothes$Age)
 
 ##group the age
-age_group <- clothes %>%
-                   mutate(Age = case_when(Age >= 80  & Age <= 89 ~ 'eighties',
-                                          Age >= 70  & Age <= 79 ~ 'seventies',
-                                          Age >= 60  & Age <= 69 ~ 'sixties',
-                                          Age >= 50  & Age <= 59 ~ 'fifties',
-                                          Age >= 40  & Age <= 49 ~ 'fourties',
-                                          Age >= 30  & Age <= 39 ~ 'thirties',
-                                          Age >= 20  & Age <= 29 ~ 'twenties'))
-
-age_group$'thirties'
   
- 
-# Most purchase and its rate.
+age_group <- clothes %>%
+                  group_by(Age) %>% 
+                  cut(Age, breaks=c(20, 30, 40, 50, 60, 70, 80), right = FALSE)
+                  cut(Age, breaks=c(20, 30, 40, 50, 60, 70, 80), right = FALSE, labels = FALSE)
+class(age_group) 
+
+
+# Does the type of purchase affect the rating?
 
 
 
 
-# Divide bad reviews into categorys 
+
 
 
 
